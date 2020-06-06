1d6a53a990a1c867ba7a601316f83a121314bee7
farisawan-2000 <faris@SIRAF2.localdomain>
2020-06-05 20:32:42 -0400

    types stuff

--- include/types.h
+++ include/types.h
@@ -28,11 +28,44 @@ struct VblankHandler
 
 // TODO: populate with actual values
 struct KirbyState {
-    u32 actionChange;
-    u32 action;
-    u32 unk8;
-    u32 powerupState;
-    u16 buttonPressed;
+    u32 actionChange; // 0x0
+    u32 action; // 0x4
+    u32 unk8; // 0x8
+    u32 powerupState; // 0xC
+    u32 unk10;
+    u32 unk14;
+    u32 unk18;
+    u32 unk1C;
+    s32 turnDirection; // 0x20
+    u32 unk24;
+    u32 unk28;
+    u32 unk2C;
+    u32 unk30;
+    u32 isTurning; // 0x34
+    f32 unk38;
+    u32 unk3C;
+    u32 unk40;
+    u32 unk44;
+    u32 unk48;
+    u32 unk4C;
+    u32 unk50;
+    u32 unk54;
+    u32 unk58;
+    u32 unk5C;
+    u32 unk60;
+    u32 unk64;
+    u32 unk68;
+    u32 unk6C;
+    u32 unk70;
+    u32 unk74;
+    u32 unk78;
+    u32 unk7C;
+    u32 unk80;
+    u32 unk84;
+    u32 unk88;
+    u32 unk8C;
+    u32 powerup; //0x90
+    u32 currentInhale; //0x94
 };
 
 #endif /* TYPES_H */
