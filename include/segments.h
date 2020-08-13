56b90c134a3dedcc19020b28c7ccdc0c25a6152e
farisawan-2000 <faris@S-RAF.localdomain>
2020-08-13 09:38:41 -0400

    Added more shiftability infrastructure

--- include/segments.h
+++ include/segments.h
@@ -14,3 +14,23 @@ extern u8 _ovl20SegmentEnd[];
 extern u8 _ovl20DataSegmentEnd[];
 extern u8 _ovl20SegNoloadStart[];
 extern u8 _ovl20SegNoloadEnd[];
+
+extern u8 _data_250320SegmentRomStart[];
+extern u8 _data_2A8CB0SegmentRomStart[];
+extern u8 _data_2B1510SegmentRomStart[];
+extern u8 _data_3E1400SegmentRomStart[];
+extern u8 _data_3E6BC0SegmentRomStart[];
+extern u8 _data_49F590SegmentRomStart[];
+extern u8 _data_4A0340SegmentRomStart[];
+extern u8 _data_4A3B60SegmentRomStart[];
+extern u8 _data_4AA8F0SegmentRomStart[];
+
+extern u8 _data_250320SegmentRomEnd[];
+extern u8 _data_2A8CB0SegmentRomEnd[];
+extern u8 _data_2B1510SegmentRomEnd[];
+extern u8 _data_3E1400SegmentRomEnd[];
+extern u8 _data_3E6BC0SegmentRomEnd[];
+extern u8 _data_49F590SegmentRomEnd[];
+extern u8 _data_4A0340SegmentRomEnd[];
+extern u8 _data_4A3B60SegmentRomEnd[];
+extern u8 _data_4AA8F0SegmentRomEnd[];
