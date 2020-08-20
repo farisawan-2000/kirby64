6ae8d3acf406af2b5ebaba9c93518e0b1b7a22fc
farisawan-2000 <faris@S-RAF.localdomain>
2020-08-20 18:14:43 -0400

    finished first run decomp of ovl18_5

--- src/ovl18/ovl18_5.h
+++ src/ovl18/ovl18_5.h
@@ -1,9 +1,14 @@
 extern s32 D_8022AA84;
 extern f32 D_8022BC40;
 extern f32 D_8022BC44;
+extern f32 D_8022BC48;
 extern f32 D_800E3AD0[];
 extern f32 D_800E3050[];
 extern f32 D_800E3590[];
 extern f32 D_800E3750[];
 extern f32 D_8022BC50;
 extern f32 D_8022BC4C;
+extern s32 D_8022AACC;
+extern s32 D_8022AAA8;
+
+extern s32 D_8022A628, D_8022A5BC;
