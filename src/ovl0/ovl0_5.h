7fa179a4b6db017cac59c69057d13f20a04d106f
farisawan-2000 <faris@S-RAF.localdomain>
2020-08-30 15:55:52 -0400

    80103004 work

--- src/ovl0/ovl0_5.h
+++ src/ovl0/ovl0_5.h
@@ -11,4 +11,16 @@ extern f32 vec3_mag_square(Vector *);
 
 #define VEC_DOT_FIRST_ARG_NEGATE(v, w) ((-v->x * w->x) + (-v->y * w->y) + (-v->z * w->z))
 
+#define VEC_SUB(result, a, b)\
+result.x = a.x - b.x;\
+result.y = a.y - b.y;\
+result.z = a.z - b.z;
+
+#define VEC_ADD(result, a, b)\
+result.x = a.x + b.x;\
+result.y = a.y + b.y;\
+result.z = a.z + b.z;
+
+#define IS_ZERO_VECTOR(v) (v.x == 0.0f && v.y == 0.0f && v.z == 0.0f)
+
 #endif
