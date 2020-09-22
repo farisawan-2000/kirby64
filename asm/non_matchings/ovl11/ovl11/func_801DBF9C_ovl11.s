d84b64021c52931cea769f893243335be1d3931b
farisawan-2000 <faris@S-RAF.localdomain>
2020-09-21 22:37:58 -0400

    cleaned up a ton of unused files

--- asm/non_matchings/ovl11/ovl11/func_801DBF9C_ovl11.s
+++
@@ -1,3 +0,0 @@
-glabel func_801DBF9C_ovl11
-/* 1E685C 801DBF9C 03E00008 */  jr    $ra
-/* 1E6860 801DBFA0 AFA40000 */   sw    $a0, ($sp)
--- asm/non_matchings/ovl18/ovl18_5/jtbl_8022BC58.s
+++
@@ -1,6 +0,0 @@
-glabel jtbl_8022BC58
-.word L80225BA8_ovl18
-.word L80225BA8_ovl18
-.word L80225BBC_ovl18
-.word L80225BD0_ovl18
-.word L80225BD0_ovl18
