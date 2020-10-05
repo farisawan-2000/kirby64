27e3cd9152d8371b3877fe395b080476bfdd434a
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2020-10-05 12:25:55 -0400

    Matching in ovl1_3, cleaned up floats in data file 039DF0, worked on yakulib light function

--- asm/non_matchings/ovl1/ovl1_3/func_800AB390.s
+++
@@ -1,3 +0,0 @@
-glabel func_800AB390
-/* 0535E0 800AB390 03E00008 */  jr    $ra
-/* 0535E4 800AB394 AFA40000 */   sw    $a0, ($sp)
--- asm/non_matchings/ovl1/ovl1_3/func_800AB478.s
+++
@@ -1,3 +0,0 @@
-glabel func_800AB478
-/* 0536C8 800AB478 03E00008 */  jr    $ra
-/* 0536CC 800AB47C AFA40000 */   sw    $a0, ($sp)
