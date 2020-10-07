d4a4a958e3cadf68f49c8adf152f681355f669cd
farisawan-2000 <farisawan.2000@gmail.com>
2020-10-07 17:30:07 -0400

    matched some of ovl1_6

---
+++ include/unk_structs/D_800DE350.h
@@ -0,0 +1,8 @@
+
+
+struct UnkStruct800DE350 {
+	u8 filler[0x4C];
+	s32 unk4C;
+};
+
+extern struct UnkStruct800DE350 *D_800DE350[];
---
+++ src/ovl1/ovl1_6.h
@@ -0,0 +1,145 @@
+#ifndef OVL1_5_H
+#define OVL1_5_H
+
+#include "ovl2/ovl2_6.h"
+
+
+extern s32 D_800DD710[];
+extern s32 D_800DEC10[];
+extern s32 D_800DEA50[];
+extern s32 D_800DE890[];
+extern s32 D_800DE6D0[];
+extern s32 D_800DE510[];
+extern s32 D_800DE190[];
+extern s32 D_800DDFD0[];
+extern s32 D_800DDE10[];
+extern s32 D_800DDC50[];
+extern s32 D_800DD8D0[];
+extern s32 D_800E0810[];
+
+extern f32 D_800E0B90[];
+extern f32 D_800E09D0[];
+extern s32 D_800E0F10[];
+extern s32 D_800E0D50[];
+extern s32 D_800E0490[];
+extern s32 *D_800E0650[];
+extern s32 *D_800E1B50[];
+extern s32 D_800E1ED0[];
+extern s32 D_800DEDD0[];
+extern s32 *D_800DEF90[];
+extern s32 *D_800DF150[];
+extern s32 D_800DF850[];
+extern s32 D_800DF690[];
+extern s32 D_800DF4D0[];
+extern s32 D_800E0110[];
+extern s32 D_800DFF50[];
+extern s32 D_800E02D0[];
+extern s32 D_800DFBD0[];
+extern f32 D_800E2410[];
+extern f32 D_800E2250[];
+extern f32 D_800E2090[];
+
+extern f32 D_800E2E90[];
+extern f32 D_800E2CD0[];
+extern f32 D_800E2B10[];
+extern f32 D_800E2950[];
+extern f32 D_800E2790[];
+extern f32 D_800E25D0[];
+extern f32 D_800E3910[];
+extern f32 D_800E3750[];
+extern f32 D_800E3590[];
+extern f32 D_800E33D0[];
+extern f32 D_800E3210[];
+extern f32 D_800E3050[];
+extern f32 D_800E3E50[];
+extern f32 D_800E3C90[];
+extern f32 D_800E3AD0[];
+extern f32 D_800E4390[];
+extern f32 D_800E41D0[];
+extern f32 D_800E4010[];
+extern f32 D_800E1450[];
+extern f32 D_800E1290[];
+extern f32 D_800E10D0[];
+extern f32 D_800E48D0[];
+extern f32 D_800E4710[];
+extern f32 D_800E4550[];
+extern f32 D_800E5DD0[];
+extern f32 D_800E5C10[];
+extern f32 D_800E5A50[];
+extern f32 D_800E4E10[];
+extern f32 D_800E4C50[];
+extern f32 D_800E4A90[];
+extern f32 D_800E5350[];
+extern f32 D_800E5190[];
+extern f32 D_800E4FD0[];
+
+extern s32 D_800E7650[];
+extern s32 D_800E8AE0[];
+extern s32 D_800E8220[];
+extern s32 D_800E83E0[];
+extern s32 D_800E8760[];
+extern s32 D_800E8E60[];
+extern f32 D_800E9020[];
+extern s32 D_800E76C0[];
+extern s32 D_800E7730[];
+
+extern u16 D_800E77A0[];
+
+extern s32 D_800E7880[];
+extern s32 D_800E78F0[];
+extern s32 D_800E7B20[];
+extern f32 D_800E7CE0[];
+extern s32 D_800EA520[];
+extern s32 D_800EA360[];
+extern s32 D_800EA1A0[];
+extern s32 D_800E9FE0[];
+extern s32 D_800E9E20[];
+extern s32 D_800E9C60[];
+extern s32 *D_800E9AA0[];
+extern s32 D_800E98E0[];
+extern f32 D_800EB320[];
+extern f32 D_800EB160[];
+extern f32 D_800EAFA0[];
+extern f32 D_800EADE0[];
+extern f32 D_800EAC20[];
+extern f32 D_800EAA60[];
+extern f32 D_800EA8A0[];
+extern f32 D_800EA6E0[];
+extern s32 D_800EB6A0[];
+extern s32 D_800EB4E0[];
+extern f32 D_800EBA20[];
+extern f32 D_800EB860[];
+extern s32 D_800EC120[];
+extern s32 D_800EBF60[];
+extern s32 D_800EBDA0[];
+extern s32 D_800EBBE0[];
+extern s32 D_800EC4A0[];
+extern s32 D_800EC2E0[];
+extern f32 D_800EC820[];
+extern f32 D_800EC660[];
+extern s32 D_800E6150[];
+extern s32 D_800E5F90[];
+extern f32 D_800E6D90[];
+extern f32 D_800E6BD0[];
+extern f32 D_800E17D0[];
+extern f32 D_800E1610[];
+extern f32 D_800E6A10[];
+extern f32 D_800E6690[];
+extern f32 D_800E64D0[];
+
+extern f32 D_800E6850[];
+extern s32 D_800E8CA0[];
+extern s32 D_800E6310[];
+extern f32 D_800E56D0[];
+extern f32 D_800E5510[];
+extern f32 D_800E5890[];
+extern s32 D_800E9720[];
+extern s32 D_800E9560[];
+extern s32 D_800E93A0[];
+
+extern f32 D_800D6B10;
+
+extern struct Normal D_800E6F50[];
+extern f32 D_800D66FC, D_800D66F8;
+
+#endif
