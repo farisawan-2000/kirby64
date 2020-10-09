cd9303c54a446c0d06d2c489b3c3a740fae3e17a
farisawan-2000 <farisawan.2000@gmail.com>
2020-10-09 11:02:32 -0400

    updated diff.py

--- diff_settings.py
+++ diff_settings.py
@@ -2,14 +2,8 @@
 
 def add_custom_arguments(parser):
     group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument('-j', dest='lang', action='store_const', const='jp',
-            help="Set version to JP.")
     group.add_argument('-u', dest='lang', action='store_const', const='us',
             help="Set version to US.")
-    group.add_argument('-e', dest='lang', action='store_const', const='eu',
-            help="Set version to EU.")
-    group.add_argument('-s', dest='lang', action='store_const', const='sh',
-            help="Set version to SH.")
 
 def apply(config, args):
     lang = args.lang or 'us'
