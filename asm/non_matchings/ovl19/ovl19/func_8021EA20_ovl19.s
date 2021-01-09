324adcae8997d1a89ab8b5be6c7447560d70f5a2
farisawan-2000 <farisawan.2000@gmail.com>
2021-01-09 01:34:08 -0500

    backup before splitting ovl1_8

--- asm/non_matchings/ovl19/ovl19/func_8021EA20_ovl19.s
+++
@@ -1,9 +0,0 @@
-glabel func_8021EA20_ovl19
-/* 23F130 8021EA20 27BDFFE8 */  addiu $sp, $sp, -0x18
-/* 23F134 8021EA24 AFBF0014 */  sw    $ra, 0x14($sp)
-/* 23F138 8021EA28 0C02D8F0 */  jal   func_800B63C0
-/* 23F13C 8021EA2C 00000000 */   nop   
-/* 23F140 8021EA30 8FBF0014 */  lw    $ra, 0x14($sp)
-/* 23F144 8021EA34 27BD0018 */  addiu $sp, $sp, 0x18
-/* 23F148 8021EA38 03E00008 */  jr    $ra
-/* 23F14C 8021EA3C 00000000 */   nop   
