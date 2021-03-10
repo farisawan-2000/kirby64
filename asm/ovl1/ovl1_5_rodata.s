aedffb57f1c418c8f883976d278234f3c13db924
farisawan-2000 <farisawan.2000@gmail.com>
2021-03-09 22:33:49 -0500

    ovl1_5 rodata

---
+++ asm/ovl1/ovl1_5_rodata.s
@@ -0,0 +1,25 @@
+.include "macros.inc"
+.section .rodata
+
+glabel jtbl_800D6680
+.word L800ACA5C
+.word L800ACA8C
+.word L800ACABC
+.word L800ACAF8
+.word L800ACAF8
+
+glabel jtbl_800D6694
+.word L800AD264
+.word L800AD3D0
+.word L800AD860
+.word L800AD89C
+.word L800AD8D8
+
+glabel jtbl_800D66A8
+.word L800AD9EC
+.word L800ADA9C
+.word L800ADB28
+.word L800ADB88
+.word L800ADC18
+
+# .skip 4
