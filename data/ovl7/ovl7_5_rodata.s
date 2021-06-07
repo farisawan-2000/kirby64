d1c23db7c0108f1886fff36818d52e61bfbe667c
farisawan-2000 <farisawan.2000@gmail.com>
2021-06-06 21:01:47 -0400

    split ovl7 rodata for ovl7_5

---
+++ data/ovl7/ovl7_5_rodata.s
@@ -0,0 +1,201 @@
+.include "macros.inc"
+.section .rodata
+
+glabel D_801CE020
+.asciiz "JL_CatchOver: No CatchInfo Address ID:%x\n"
+.balign 4
+
+glabel D_801CE04C
+.word 0x477FFF00
+
+glabel D_801CE050
+.word 0x40490FDB
+
+glabel D_801CE054
+.word 0x3ECCCCCD
+
+glabel D_801CE058
+.word 0x3E99999A
+
+glabel D_801CE05C
+.word 0x3ECCCCCD
+
+glabel D_801CE060
+.word 0x3ECCCCCD
+
+glabel D_801CE064
+.word 0x3EB2B8C3
+
+glabel D_801CE068
+.word 0x40C90FDB
+
+glabel D_801CE06C
+.word 0x3F32B8C3
+
+glabel D_801CE070
+.word 0x3FC90FDB
+
+glabel D_801CE074
+.word 0xBD8EFA35
+
+glabel D_801CE078
+.word 0x40B2B8C3
+
+glabel D_801CE07C
+.word 0x3FC90FDB
+
+glabel D_801CE080
+.word 0x3D8EFA35
+
+glabel D_801CE084
+.word 0x3F666666
+
+glabel D_801CE088
+.word 0x3F4CCCCD
+
+glabel D_801CE08C
+.word 0x477FFF00
+
+glabel jtbl_801CE090
+.word 0x801A90AC
+.word 0x801A90B4
+.word 0x801A90B4
+.word 0x801A90B4
+.word 0x801A9074
+.word 0x801A9074
+.word 0x801A90B4
+.word 0x801A90B4
+.word 0x801A90B4
+.word 0x801A9064
+.word 0x801A906C
+
+glabel D_801CE0BC
+.word 0x801A9774
+.word 0x801A9774
+.word 0x801A9774
+.word 0x801A9774
+.word 0x801A9774
+.word 0x801A976C
+.word 0x801A976C
+.word 0x801A976C
+.word 0x801A976C
+.word 0x801A9774
+
+glabel D_801CE0E4
+.word 0x801A9B00
+.word 0x801A9B54
+.word 0x801A9A8C
+.word 0x801A9B54
+.word 0x801A9B20
+.word 0x801A9B54
+.word 0x801A9A58
+.word 0x801A9A24
+.word 0x801A9A38
+.word 0x801A9B54
+.word 0x801A9B54
+.word 0x801A9AC8
+
+glabel D_801CE114
+.word 0x3A83126F
+
+glabel D_801CE118
+.word 0x3A83126F
+
+glabel D_801CE11C
+.word 0x40490FDB
+
+glabel D_801CE120
+.word 0x3D8EFA35
+
+glabel D_801CE124
+.word 0x477FFF00
+
+glabel D_801CE128
+.word 0x477FFF00
+
+glabel D_801CE12C
+.word 0x40C90FDB
+
+glabel D_801CE130
+.word 0x3EB2B8C3
+
+glabel D_801CE134
+.word 0x3F32B8C3
+
+glabel D_801CE138
+.word 0x3FC90FDB
+
+glabel D_801CE13C
+.word 0xBD8EFA35
+
+glabel D_801CE140
+.word 0x40B2B8C3
+
+glabel D_801CE144
+.word 0x3FC90FDB
+
+glabel D_801CE148
+.word 0x3D8EFA35
+
+glabel D_801CE14C
+.word 0x3D8EFA35
+
+glabel D_801CE150
+.word 0x3D8EFA35
+
+glabel D_801CE154
+.word 0x477FFF00
+
+glabel D_801CE158
+.word 0x477FFF00
+
+glabel D_801CE15C
+.word 0x40490FDB
+
+glabel D_801CE160
+.word 0x456E656D
+
+glabel D_801CE164
+.word 0x79205368
+
+glabel D_801CE168
+.word 0x6F742052
+
+glabel D_801CE16C
+.word 0x65717565
+
+glabel D_801CE170
+.word 0x73742045
+
+glabel D_801CE174
+.word 0x72726F72
+
+glabel D_801CE178
+.word 0x210A0000
+
+glabel D_801CE17C
+.word 0x456E656D
+
+glabel D_801CE180
+.word 0x79205368
+
+glabel D_801CE184
+.word 0x6F742054
+
+glabel D_801CE188
+.word 0x7261636B
+
+glabel D_801CE18C
+.word 0x204F7665
+
+glabel D_801CE190
+.word 0x72210A00
+
+glabel D_801CE194
+.word 0x00000000
+
+glabel D_801CE198
+.word 0x00000000
+
+glabel D_801CE19C
+.word 0x00000000
