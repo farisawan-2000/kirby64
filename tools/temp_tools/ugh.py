c5250a8e05e1bf6f602dc0a2dcaa31c040f4b28c
someone2639 <someone2639@gmail.com>
2022-12-26 22:19:17 -0500

    ovl12 powered by spimdisasm

--- tools/temp_tools/ugh.py
+++ tools/temp_tools/ugh.py
@@ -2,4 +2,4 @@ import sys, os
 with open(sys.argv[1]) as f:
 	for line in f:
 		tok = line.split()
-		os.system("./rename_sym.sh "+tok[0]+" "+tok[1])
+		os.system("grep -rl %s %s | xargs sed -i 's/%s/%s/g'" % (tok[0], sys.argv[2], tok[0], tok[1]))
